2bbb47dcb5360f3993a4d7b0c38ea8e919ae2585
Rishabh Bajpai <rishabh.bajpai@outlook.com>
2014-10-09 09:09:51 -0700

    Added all the header files

--- FantasySportsApplication/FantasySportsApplication.cpp
+++ FantasySportsApplication/FantasySportsApplication.cpp
@@ -1,11 +1,9 @@
-// FantasySportsApplication.cpp : Defines the entry point for the console application.
-//
-
-#include "stdafx.h"
-
-
-int _tmain(int argc, _TCHAR* argv[])
-{
+#include <iostream>
+#include "stdafx.h" //remove this line if not using visual studio
+int main(int argc, const char * argv []) {
+	//Get vector of players from CSV. -- see Player.h then QB.h need to add all positions
+	//Create player bucket -- see PlayerBucket.h
+	//Create Team instance -- see Team.h
+	//Blah blah
 	return 0;
 }
-
