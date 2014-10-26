f238783fadb8b8700f8130db044418360ccd25b4
Dennis Berko <dennisberko@gmail.com>
2014-10-26 09:29:41 -0700

    Test files wont collide

---
+++ FantasySportsApplication/HashTable/HashTableTest.h
@@ -0,0 +1,22 @@
+//
+//  test.h
+//  HashTable
+//
+//  Created by Dennis Berko on 10/25/14.
+//  Copyright (c) 2014 Dennis. All rights reserved.
+//
+
+#ifndef HASHTABLETEST_H
+#define HASHTABLETEST_H
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include "HashTable.h"
+
+using namespace std;
+
+void testHashTable();
+void printHashTableEntry(string &item);
+
+#endif /* defined(__HashTable__test__) */
