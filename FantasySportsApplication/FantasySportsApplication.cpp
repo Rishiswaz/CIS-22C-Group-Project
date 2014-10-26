af3de38a7cde19a1b1b3eebd4b58087d9344f452
Dennis Berko <dennisberko@gmail.com>
2014-10-26 09:30:50 -0700

    Remove the use of precompiled headers

--- FantasySportsApplication/FantasySportsApplication.cpp
+++ FantasySportsApplication/FantasySportsApplication.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
-#include "stdafx.h" //remove this line if not using visual studio
 int main(int argc, const char * argv []) {
-	//Get vector of players from CSV. -- see Player.h then QB.h need to add all positions
-	//Create player bucket -- see PlayerBucket.h
-	//Create Team instance -- see Team.h
-	//Blah blah
+
 	return 0;
 }
