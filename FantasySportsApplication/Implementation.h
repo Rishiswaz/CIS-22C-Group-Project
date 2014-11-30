f760403bf3fe7d85e112909d353590088a65c188
Rishabh Bajpai <rishabh.bajpai@outlook.com>
2014-11-29 16:51:57 -0800

    added some #includes and put in the framework for the IO

--- FantasySportsApplication/Implementation.h
+++ FantasySportsApplication/Implementation.h
@@ -6,3 +6,43 @@
 #include "LinkedList.h"
 #include "Team\Team.h"
 #include "bst.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <ostream>
+#include <strstream>
+
+
+/*
+void runInput(std::string inputFileName, std::vector<Person>& people)
+{
+int inDay, inMonth, inYear,i=0;
+Birthday inBday;
+char separator;
+std::string inName;
+std::ifstream myfile;
+myfile.open(inputFileName);
+while (myfile.good())
+{
+
+myfile >> inDay;
+
+myfile >> separator;
+
+myfile >> inMonth;
+
+myfile >> separator;
+
+myfile >> inYear;
+
+myfile >> separator;
+
+std::getline(myfile, inName, '\n');
+// Ignore the newline, as it is still in the buffer.
+myfile.ignore(10000, '\n');
+people[i].insertValues(inBday, inName);
+i += 1;
+}
+myfile.close();
+}
+*/
