0632aaca71cc5979ac565b27f6f41513ef58c6e3
mkr123 <mankairui@yahoo.com>
2014-12-01 23:21:03 -0800

    Create main.cpp
    This file use for testing the BST, which finally works well.  I just need fileI/O to input data to make it work with our project. Currently it can insert an element,delete element,search an element with a key,find highest or lowest element of the key value ,print out all elements from low to high.

---
+++ FantasySportsApplication/BST/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "binaryTree.h"
+#include "binaryTree.cpp"
+#include <fstream>
+#include <string>
+using namespace binarytree;
+using namespace std;
+int main()
+{
+	int keylist[5] = { 333, 111, 222, 444, 555 };
+	CBinaryTree<int> teamtree;
+	CTreeNode<int> *searchteam;
+	for (int i = 0; i < 5; i++)
+	{
+		teamtree.insertElement(keylist[i]);
+	}
+	
+	teamtree.deleteValue(555);
+	searchteam = teamtree.search(333);
+	cout << searchteam->key << endl;;
+	
+	 
+	 teamtree.inorderTree(teamtree.root);
+
+
+	system("pause");
+	return 1;
+}
