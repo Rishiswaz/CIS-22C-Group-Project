db4a1686e4f866c248a43ee97d49e938f2d4b1b0
Rishabh Bajpai <rishabh.bajpai@outlook.com>
2014-12-04 13:36:26 -0800

    dont synch this commit please

--- FantasySportsApplication/Team/Team.h
+++ FantasySportsApplication/Team/Team.h
@@ -18,136 +18,137 @@ public:
 		return yards;
 	}
 	
-	auto keyOutput(char keyVal)
+	std::string keyOutput(char keyVal)
+	{
+		if (keyVal == 'n')
+			return teamName;
+		return " ";
+	};
+
+	int keyOutput(char keyVal, int retInt)
 	{
 		switch (keyVal)
 		{
-		case 'w':return wins;
+		case 'w':return ;
 			break;
-		case 'l':return losses;
+		case 'l':return;
 			break;
-		case 'p':return PPI;
+		case 'p':return;
 			break;
-		case 'n':return teamName;
-			break;
-		default:
-			std::cout << "An error occured parsing your input, default key will be used" << std::endl;
-			break;
+
 		}
-		return yards;
-	};
-	//to access private members or this class from fileIO
-	void input(std::string t, int w, int l, int y, double p, int d)
-	{
-		wins = w;
-		losses = l;
-		yards = y;
-		percentage = p;
-		division = d;
-		teamName = t;
-		setRank();
-	}
-	std::string getDiv()
-	{
-		std::string returnValue;
-		switch (division)
+
+		//to access private members or this class from fileIO
+		void input(std::string t, int w, int l, int y, double p, int d)
 		{
-		case 1: returnValue = "NFC North";
-			break;
-		case 2: returnValue = "NFC East";
-			break;
-		case 3: returnValue = "NFC South";
-			break;
-		case 4: returnValue = "NFC West";
-			break;
-		case 5: returnValue = "AFC North";
-			break;
-		case 6: returnValue = "AFC East";
-			break;
-		case 7: returnValue = "AFC South";
-			break;
-		case 8: returnValue = "AFC West";
-			break;
-		default: std::cerr << "There was an error reading the division"<<std::endl
-			<<"Please make sure that the division value for "<<teamName<<" is valid.";
-			break;
+			wins = w;
+			losses = l;
+			yards = y;
+			percentage = p;
+			division = d;
+			teamName = t;
+			setRank();
 		}
-		return returnValue;
-	}
-	//Rank manipulation
-	void setRank()
-	{
-		double value;
-		value = yards*percentage;
-		trunc(value);
-		PPI = value;
-	}//this will set the playoffIndexScore to position it on the Playoff Performance Index
-					//use this after changing any values or updating any data
-	//V-overloaded = operator defined inline (rhs: Right Hand Side)-V
-	Team & operator=(const Team& rhs) 
-	{
-//LHS (destination)    =     RHS (source)
-		Team dummy;
-		dummy.wins = rhs.wins;
-		dummy.losses = rhs.yards;
-		dummy.yards = rhs.yards;
-		dummy.percentage = rhs.percentage;
-		dummy.PPI = rhs.PPI;
-		dummy.division = rhs.division;
-		dummy.percentage = rhs.percentage;
-		dummy.teamName = rhs.teamName;
-		return dummy;
-	};
-	friend std::ostream& operator<<(std::ostream& os, Team& dt)
-	{
-		os << dt.teamName << " " << dt.wins << " Wins, " << dt.losses << " Losses, " << std::endl
-			<< "In "<<dt.getDiv()<< " with a PPI of "<< dt.PPI<<std::endl<<std::endl;
-		return os;
-	}
-	int getASCII()
-	{
-		stringASCII = 0;
-		for (int i = 0; i < teamName.length(); i++)
+		std::string getDiv()
 		{
-			stringASCII += int(teamName.at(i));
+			std::string returnValue;
+			switch (division)
+			{
+			case 1: returnValue = "NFC North";
+				break;
+			case 2: returnValue = "NFC East";
+				break;
+			case 3: returnValue = "NFC South";
+				break;
+			case 4: returnValue = "NFC West";
+				break;
+			case 5: returnValue = "AFC North";
+				break;
+			case 6: returnValue = "AFC East";
+				break;
+			case 7: returnValue = "AFC South";
+				break;
+			case 8: returnValue = "AFC West";
+				break;
+			default: std::cerr << "There was an error reading the division" << std::endl
+				<< "Please make sure that the division value for " << teamName << " is valid.";
+			}
+			return returnValue;
 		}
-		return stringASCII;
-	}
-	void changeVals(int choice, int val)
-	{
-		switch (choice)
+		//Rank manipulation
+		void setRank()
 		{
-		case 1: yards += val;
-			break;
-		case 2: yards -= val;
-			break;
-		case 3: wins += val;
-			break;
-		case 4: wins -= val;
-			break;
-		case 5: losses += val;
-			break;
-		case 6: losses -= val;
-			;
-			break;
-		default:
-			break;
+			double value;
+			value = yards*percentage;
+			trunc(value);
+			PPI = value;
+		}//this will set the playoffIndexScore to position it on the Playoff Performance Index
+						//use this after changing any values or updating any data
+		//V-overloaded = operator defined inline (rhs: Right Hand Side)-V
+		Team & operator=(const Team& rhs)
+		{
+			//LHS (destination)    =     RHS (source)
+			Team dummy;
+			dummy.wins = rhs.wins;
+			dummy.losses = rhs.yards;
+			dummy.yards = rhs.yards;
+			dummy.percentage = rhs.percentage;
+			dummy.PPI = rhs.PPI;
+			dummy.division = rhs.division;
+			dummy.percentage = rhs.percentage;
+			dummy.teamName = rhs.teamName;
+			return dummy;
 		};
-	};
-	void editStuff(int win, int loss, std::string name)
-	{
-		wins = win;
-		losses = loss;
-		teamName = name;
-		percentage = win / loss;
-		setRank();
+		friend std::ostream& operator<<(std::ostream& os, Team& dt)
+		{
+			os << dt.teamName << " " << dt.wins << " Wins, " << dt.losses << " Losses, " << std::endl
+				<< "In " << dt.getDiv() << " with a PPI of " << dt.PPI << std::endl << std::endl;
+			return os;
+		}
+		int getASCII()
+		{
+			stringASCII = 0;
+			for (int i = 0; i < teamName.length(); i++)
+			{
+				stringASCII += int(teamName.at(i));
+			}
+			return stringASCII;
+		}
+		void changeVals(int choice, int val)
+		{
+			switch (choice)
+			{
+			case 1: yards += val;
+				break;
+			case 2: yards -= val;
+				break;
+			case 3: wins += val;
+				break;
+			case 4: wins -= val;
+				break;
+			case 5: losses += val;
+				break;
+			case 6: losses -= val;
+				;
+				break;
+			default:
+				break;
+			};
+		};
+		void editStuff(int win, int loss, std::string name)
+		{
+			wins = win;
+			losses = loss;
+			teamName = name;
+			percentage = win / loss;
+			setRank();
+		}
+	private:
+		
+		int wins, losses, yards, PPI, division, stringASCII;
+		double percentage;
+		std::string  teamName;
 	}
-private:
-	//explanation for values will be added in a txt file (ie:frDownPerGame: First Downs Per Game)
-	int wins, losses, yards, PPI, division,stringASCII;
-	double percentage;
-	std::string teamName;
-	
 };
 
 Team::Team(std::string name, int win, int loss, int yard, double percent, int div)
